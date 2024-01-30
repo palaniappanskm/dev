f8f630cbf8de9448feca775c47951ee301745748
palaniappanskm <143245308+palaniappanskm@users.noreply.github.com>
2024-01-30 17:27:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 # Deploy Docker image to server
-docker-compose up -d
+#docker-compose up -d
+#!/bin/bash
+
+# This script performs the deployment steps for your application
+
+# Navigate to project directory
+cd https://github.com/palaniappanskm/dev.git
+
+# Example: Copy build artifacts to the deployment directory on the server using SCP
+scp -r build/ user@server: http://server_ip:8080
+
+# Example: Restart the application server
+ssh user@server "sudo systemctl restart your_application_service"
 
