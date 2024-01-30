a73050c35ab4b608ca27f56772c930c5d5506827
palaniappanskm <143245308+palaniappanskm@users.noreply.github.com>
2024-01-30 17:20:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 # Build Docker image
-docker build -t react-app .
+#docker build -t react-app .
+#!/bin/bash
+
+# This script performs the build steps for your project
+
+# Navigate to project directory
+cd https://github.com/palaniappanskm/dev.git
+
+# Execute any build commands, for example:
+npm install
+npm run build
 
